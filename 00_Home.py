395b2b3496e55b91d68d534394df7f0d87e72053
Carlos Eduardo Veras Neves <carlosneves@macbook-2.local>
2024-05-14 22:35:41 -0300

    construção do esqueleto do aplicativo streamlit

---
+++ 00_Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: white;'>Tech Challenge 04</h1>", unsafe_allow_html=True)
+
+st.image('images/img_fiap.jpeg', caption='FIAP -Alura Pós-Tech - 3DTAT (maio/2024)', use_column_width=True)
+
+st.markdown('''
+            ## Análise da série de Preços do Petróleo do tipo Brent 
+            ---
+
+            ### Elaborado por:
+            - Carlos Eduardo Veras Neves - rm 353068
+            
+            ''')
