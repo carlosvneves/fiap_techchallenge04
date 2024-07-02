e1a4bd27ecc6f725df3e6ef18fe171f3cc782ea5
CARLOS EDUARDO NEVES <carlos.vneves@gmail.com>
2024-07-01 23:29:11 -0300

    autogluon predition - low wql

--- pages/03_Modelos.py
+++ pages/03_Modelos.py
@@ -3,16 +3,20 @@ import streamlit as st
 import utils.prophet_model as prophet_model
 import utils.autogluon_model as autogluon_model
 
+import plotly.express as px
+import plotly.graph_objects as go
+
 # load data
-petr_brent = st.session_state.petr_brent
+# petr_brent = st.session_state.petr_brent
 
 st.markdown(
-    "<h1 style='text-align: center;'> Modelos Preditivos </h1>",
-    unsafe_allow_html=True
+    "<h1 style='text-align: center;'> Modelos Preditivos </h1>", unsafe_allow_html=True
 )
 
 # build tabs
-tab1, tab2, tab3 = st.tabs(["Prophet", "Autogluon - Multivariado", "XGBoost - Multivariado"])
+tab1, tab2, tab3 = st.tabs(
+    ["Prophet", "Autogluon - Multivariado", "XGBoost - Multivariado"]
+)
 
 # prophet model
 with tab1:
@@ -20,7 +24,7 @@ with tab1:
     st.markdown("""---""")
 
     model, forecast = prophet_model.prophet_model()
-    
+
     st.plotly_chart(prophet_model.get_forecast_plotly(model, forecast))
 
     st.markdown("""---""")
@@ -33,22 +37,54 @@ with tab1:
 
 
 with tab2:
-    import matplotlib.pyplot as plt 
-    
-    st.write("Autogluon - Multivariado")
 
     with st.spinner("Carregando o modelo..."):
-        predictor, df_mult_train, df_mult_test = autogluon_model.autogluon_model(load=True)
+        predictor, df_mult_train, df_mult_test = autogluon_model.autogluon_model(
+            load=True
+        )
     # insample predictions
     with st.spinner("Realizando as previsões (dentro da amostra)..."):
-        predictions = autogluon_model.predict(df_mult_train, known_covariates=df_mult_test, predictor=predictor)
-    st.write(predictions)
-     
-    # fig, ax = plt.figure(figsize=(10, 9))
-    # autogluon_model.get_forecast_chart(df_mult_test, predictions, predictor)
+        predictions = autogluon_model.predict(
+            df_mult_train, known_covariates=df_mult_test, predictor=predictor
+        )
 
-    st.pyplot(autogluon_model.get_forecast_chart(df_mult_test, predictions, predictor), )
-        
+    st.markdown("## Previsões (dentro da amostra - 180 dias):")
+
+    st.plotly_chart(
+        autogluon_model.get_forecast_plotly(
+            test_data=df_mult_test, predictions=predictions
+        )
+    )
+
+    st.markdown("### Importância de cada modelo no _ensemble_:")
+    
+    st.write(predictor.fit_summary().get("leaderboard"))
+
+    st.markdown("### Métricas de avaliação do modelo:")
+
+    metrics = predictor.evaluate(
+            df_mult_test, metrics=["MAPE", "RMSE", "MSE", "MAE", "RMSSE", "MASE", "SQL"]
+        )
+    metrics = pd.DataFrame(metrics.values(), index=metrics.keys())
+    st.write(
+        metrics
+    )
+
+    st.markdown("## Previsão (fora da amostra - 180 dias):")
+
+    future_data = autogluon_model.make_future_data(df_mult_test, fh=180)
+
+    predictions_out_of_sample = autogluon_model.predict(
+        df_mult_test, known_covariates=future_data, predictor=predictor
+    )
+
+    # st.write(predictions_out_of_sample)
+
+    st.plotly_chart(
+        autogluon_model.get_forecast_plotly(
+            test_data=df_mult_test, predictions=predictions_out_of_sample
+        )
+    )
 
 with tab3:
     st.write("XGBoost - Multivariado")
--- utils/autogluon_model.py
+++ utils/autogluon_model.py
@@ -4,6 +4,12 @@ from autogluon.timeseries.utils.forecast import get_forecast_horizon_index_ts_da
 
 from os import path
 
+
+import plotly.graph_objects as go
+
+# implementar outras features como aqui?
+# https://plbalmeida.gitbook.io/fiap-hands-on-de-ml-para-series-temporais/parte-1/overview/previsao-de-preco-d+15/performance_ml_d7
+
 def create_features(df):
     """
     Create time series features based on time series index.
@@ -15,6 +21,7 @@ def create_features(df):
     df["month"] = df.index.month
     df["year"] = df.index.year
     df["dayofyear"] = df.index.day_of_year
+    #df["rolling_std_7"] = df["y"].shift(1).rolling(window=7).std()
     return df.reset_index()
 
 
@@ -44,7 +51,7 @@ def fit_and_test(train_data, test_data, fh=180):
         prediction_length=fh,
         path="saved_models/autogluon-petr-mult",
         target="y",
-        eval_metric="SQL",
+        eval_metric= "wQL",
         quantile_levels=[0.1, 0.25, 0.5, 0.75, 0.9],
         known_covariates_names=[
             "hour",
@@ -58,13 +65,13 @@ def fit_and_test(train_data, test_data, fh=180):
 
     predictor.fit(
         train_data,
-        presets="fast_training", #"hq",
+        #presets="hq", # caso não tenha gpu para treinar o modelo
+        presets= "chronos_large_ensemble",        
         time_limit=600,
         num_val_windows=5,
         random_seed=123,
+        
     )
-
-    #predictions = predictor.predict(train_data, known_covariates=test_data)
 
     return predictor
 
@@ -74,7 +81,15 @@ def predict(data, known_covariates, predictor, fh=180):
 def make_future_data(data, fh=180):
     
     future_index = get_forecast_horizon_index_ts_dataframe(data, prediction_length=fh)
+    future_timestamps = future_index.get_level_values("timestamp")
     known_covariates = pd.DataFrame(index=future_index)
+    known_covariates['hour'] = future_timestamps.hour
+    known_covariates['dayofweek'] = future_timestamps.day_of_week
+    known_covariates['quarter'] = future_timestamps.quarter
+    known_covariates['month'] = future_timestamps.month
+    known_covariates['year'] = future_timestamps.year
+    known_covariates['dayofyear'] = future_timestamps.day_of_year
+
 
     return known_covariates
 
@@ -88,6 +103,98 @@ def get_forecast_chart(data, predictions, predictor):
    return predictor.plot(data, predictions=predictions, quantile_levels=[0.1, 0.5, 0.9],)
 
 
+def get_forecast_plotly(test_data, predictions):
+
+    fig = go.Figure()
+
+    df_merge = pd.merge(
+        test_data, predictions.reset_index(), on="timestamp", how="outer"
+    )
+
+    # Add original values as a scatter plot
+    fig.add_trace(
+        go.Scatter(
+            x=df_merge["timestamp"],
+            y=df_merge["y"],
+            mode="lines",
+            name="Original",
+            opacity=0.3,
+            marker=dict(color="black"),
+        )
+    )
+    # Add the predicted mean line
+    fig.add_trace(
+        go.Scatter(
+            x=df_merge["timestamp"],
+            y=df_merge["mean"],
+            mode="lines",
+            name="Média da Previsão",
+            line=dict(color="royalblue"),
+        )
+    )
+    # Add the lower quantile trace using Plotly Graph Objects
+    fig.add_trace(
+        go.Scatter(
+            x=df_merge["timestamp"],
+            y=df_merge["0.1"],
+            mode="lines",
+            name="Q10",
+            line=dict(width=0),
+            showlegend=False,
+        )
+    )
+
+    # Add the upper quantile trace and fill the area between the quantiles using Plotly Graph Objects
+    fig.add_trace(
+        go.Scatter(
+            x=df_merge["timestamp"],
+            y=df_merge["0.9"],
+            mode="lines",
+            name="Q90",
+            fill="tonexty",  # fill area between quant_0.1 and quant_0.9
+            fillcolor="rgba(255,165,0,0.2)",
+            line=dict(width=0),
+            showlegend=False,
+        )
+    )
+
+    # Set the initial x-axis range to start on 2022-06-01
+    fig.update_xaxes(
+        range=["2022-01-01", df_merge.reset_index()["timestamp"].max()]
+    )
+
+    # Update layout for better presentation
+    fig.update_layout(
+        xaxis_title="Timestamp",
+        yaxis_title="Value US$",
+        title="Previsão do preço de petróleo do tipo brent",
+        template="plotly_white",
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list(
+                    [
+                        dict(count=1, label="1m", step="month", stepmode="backward"),
+                        dict(count=6, label="6m", step="month", stepmode="backward"),
+                        dict(count=1, label="1y", step="year", stepmode="backward"),
+                        dict(step="all"),
+                    ]
+                )
+            ),
+            rangeslider=dict(
+                visible=True,
+                range=[
+                    df_merge.reset_index()["timestamp"],
+                    df_merge.reset_index()["timestamp"].max(),
+                ],
+            ),
+            type="date",
+        ),
+        width=900,  # Set the width of the figure
+        height=800,  # Set the height of the figure
+    )
+    return fig 
+
+    
 def autogluon_model(fh=180, load=False):
     
     fh = 180
